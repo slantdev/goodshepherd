b6c55ba6cb13c8d4ec13c1ffb1aff66ad8f9a14f
Slant Dev <build@amazeworx.com>
2025-12-06 17:07:53 +0700

    refactor: gallery_block_02 component

--- template-parts/components/gallery_block_02.php
+++ template-parts/components/gallery_block_02.php
@@ -4,13 +4,62 @@
  * Layout: gallery_block_02
  */
 
-// $field = get_sub_field('field_name');
+$main_image = get_sub_field('image_source'); // Single Image
+$thumbnails = get_sub_field('gallery_images'); // Repeater
+
+// Styling
+$section_id    = goodshep_get_section_id();
+$block_classes = goodshep_get_block_classes();
+$bg_style      = goodshep_get_bg_image_style();
 ?>
 
-<section class="component-gallery_block_02 py-12">
+<section <?php echo $section_id; ?> class="<?php echo esc_attr( $block_classes ); ?>" style="<?php echo esc_attr( $bg_style ); ?>">
     <div class="container mx-auto px-4">
-        <div class="p-4 border-2 border-dashed border-gray-300 bg-gray-50 rounded-lg text-center">
-            <p class="text-gray-500 font-mono text-sm">Template: template-parts/components/gallery_block_02.php</p>
+        
+        <div class="grid md:grid-cols-2 gap-4 md:gap-8">
+            
+            <!-- Left: Thumbnails Grid -->
+            <div class="grid grid-cols-2 gap-4">
+                <?php 
+                if ( have_rows('gallery_images') ) :
+                    while ( have_rows('gallery_images') ) : the_row();
+                        $img = get_sub_field('image_source');
+                        if ( $img ) :
+                ?>
+                    <div class="h-40 md:h-64 w-full rounded-md overflow-hidden bg-gray-100">
+                        <?php 
+                        if ( is_array( $img ) && isset( $img['ID'] ) ) {
+                            echo wp_get_attachment_image( $img['ID'], 'large', false, array( 'class' => 'h-full w-full object-cover' ) );
+                        } elseif ( is_numeric( $img ) ) {
+                            echo wp_get_attachment_image( $img, 'large', false, array( 'class' => 'h-full w-full object-cover' ) );
+                        } elseif ( is_string( $img ) ) {
+                            echo '<img src="' . esc_url( $img ) . '" class="h-full w-full object-cover" alt="">';
+                        }
+                        ?>
+                    </div>
+                <?php 
+                        endif;
+                    endwhile;
+                endif;
+                ?>
+            </div>
+
+            <!-- Right: Main Image -->
+            <div class="h-64 md:h-full w-full rounded-md overflow-hidden bg-gray-100 min-h-[20rem]">
+                <?php 
+                if ( $main_image ) {
+                    if ( is_array( $main_image ) && isset( $main_image['ID'] ) ) {
+                        echo wp_get_attachment_image( $main_image['ID'], 'full', false, array( 'class' => 'h-full w-full object-cover' ) );
+                    } elseif ( is_numeric( $main_image ) ) {
+                        echo wp_get_attachment_image( $main_image, 'full', false, array( 'class' => 'h-full w-full object-cover' ) );
+                    } elseif ( is_string( $main_image ) ) {
+                        echo '<img src="' . esc_url( $main_image ) . '" class="h-full w-full object-cover" alt="">';
+                    }
+                }
+                ?>
+            </div>
+
         </div>
+
     </div>
 </section>
