fbfc4790c6c702f18b488927e83d7b8c4a972a71
Slant Dev <build@amazeworx.com>
2025-12-06 17:16:27 +0700

    refactor: html_block component

--- template-parts/components/html_block.php
+++ template-parts/components/html_block.php
@@ -1,16 +1,30 @@
 <?php
 /**
- * Component: Html Block
+ * Component: HTML Block
  * Layout: html_block
  */
 
-// $field = get_sub_field('field_name');
+$title = get_sub_field('title');
+$text  = get_sub_field('text');
+
+// Styling
+$section_id    = goodshep_get_section_id();
+$block_classes = goodshep_get_block_classes();
+$bg_style      = goodshep_get_bg_image_style();
 ?>
 
-<section class="component-html_block py-12">
+<section <?php echo $section_id; ?> class="<?php echo esc_attr( $block_classes ); ?>" style="<?php echo esc_attr( $bg_style ); ?>">
     <div class="container mx-auto px-4">
-        <div class="p-4 border-2 border-dashed border-gray-300 bg-gray-50 rounded-lg text-center">
-            <p class="text-gray-500 font-mono text-sm">Template: template-parts/components/html_block.php</p>
-        </div>
+        
+        <?php if ( $title ) : ?>
+            <h2 class="text-2xl mb-4 font-semibold"><?php echo esc_html( $title ); ?></h2>
+        <?php endif; ?>
+
+        <?php if ( $text ) : ?>
+            <div class="prose max-w-none text-base mb-4">
+                <?php echo $text; // Allow raw HTML ?>
+            </div>
+        <?php endif; ?>
+
     </div>
 </section>
