5a1dbb20498b6a57b56b6268e03b993834d01c2d
Slant Dev <build@amazeworx.com>
2025-12-07 13:36:01 +0700

    refactor: step_block_cards and subscribe_to_newsletter components

--- template-parts/components/subscribe_to_newsletter.php
+++ template-parts/components/subscribe_to_newsletter.php
@@ -1,16 +1,50 @@
 <?php
 /**
- * Component: Subscribe To Newsletter
+ * Component: Subscribe to Newsletter
  * Layout: subscribe_to_newsletter
  */
 
-// $field = get_sub_field('field_name');
+$title     = get_sub_field('title');
+$text      = get_sub_field('text');
+$shortcode = get_sub_field('form_shortcode');
+
+// Styling
+$section_id    = goodshep_get_section_id();
+$block_classes = goodshep_get_block_classes();
+$bg_style      = goodshep_get_bg_image_style();
 ?>
 
-<section class="component-subscribe_to_newsletter py-12">
-    <div class="container mx-auto px-4">
-        <div class="p-4 border-2 border-dashed border-gray-300 bg-gray-50 rounded-lg text-center">
-            <p class="text-gray-500 font-mono text-sm">Template: template-parts/components/subscribe_to_newsletter.php</p>
+<section <?php echo $section_id; ?> class="<?php echo esc_attr( $block_classes ); ?>" style="<?php echo esc_attr( $bg_style ); ?>">
+    <div class="container mx-auto px-4 relative">
+        
+        <div class="max-w-xl mx-auto text-center relative">
+            
+            <!-- Decorative Heart -->
+            <div class="absolute -top-4 -left-12 hidden md:block opacity-80">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/gs_heart.svg" alt="" class="h-12 w-auto">
+            </div>
+
+            <?php if ( $title ) : ?>
+                <h2 class="font-semibold text-3xl mb-4"><?php echo esc_html( $title ); ?></h2>
+            <?php endif; ?>
+
+            <?php if ( $text ) : ?>
+                <div class="text-xl leading-relaxed mb-8 text-gray-700">
+                    <?php echo wp_kses_post( $text ); ?>
+                </div>
+            <?php endif; ?>
+
         </div>
+
+        <div class="max-w-2xl mx-auto py-8 text-center">
+            <?php 
+            if ( $shortcode ) {
+                echo '<div class="form-wrapper">' . do_shortcode( $shortcode ) . '</div>';
+            } elseif ( is_user_logged_in() ) {
+                echo '<p class="p-4 bg-gray-100 border border-dashed text-gray-500">Select a form in the editor.</p>';
+            }
+            ?>
+        </div>
+
     </div>
 </section>
