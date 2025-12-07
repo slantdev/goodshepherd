6d4240a98c327b5635c12b0de21a2c6dbb1eb9ba
Slant Dev <build@amazeworx.com>
2025-12-07 14:00:14 +0700

    refactor: final components

--- template-parts/components/twitter_feed.php
+++ template-parts/components/twitter_feed.php
@@ -4,13 +4,27 @@
  * Layout: twitter_feed
  */
 
-// $field = get_sub_field('field_name');
+$title     = get_sub_field('title');
+$shortcode = get_sub_field('form_shortcode'); // Reference calls it 'form_shortcode' in get_twitter_feed_content
+
+// Styling
+$section_id    = goodshep_get_section_id();
+$block_classes = goodshep_get_block_classes();
+$bg_style      = goodshep_get_bg_image_style();
 ?>
 
-<section class="component-twitter_feed py-12">
+<section <?php echo $section_id; ?> class="<?php echo esc_attr( $block_classes ); ?>" style="<?php echo esc_attr( $bg_style ); ?>">
     <div class="container mx-auto px-4">
-        <div class="p-4 border-2 border-dashed border-gray-300 bg-gray-50 rounded-lg text-center">
-            <p class="text-gray-500 font-mono text-sm">Template: template-parts/components/twitter_feed.php</p>
-        </div>
+        
+        <?php if ( $title ) : ?>
+            <h2 class="text-red-600 font-semibold mb-4 text-3xl"><?php echo esc_html( $title ); ?></h2>
+        <?php endif; ?>
+
+        <?php if ( $shortcode ) : ?>
+            <div class="twitter-feed-container">
+                <?php echo do_shortcode( $shortcode ); ?>
+            </div>
+        <?php endif; ?>
+
     </div>
 </section>
