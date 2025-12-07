204afdc6aac8008a974a8ac94a842974592ab8da
Slant Dev <build@amazeworx.com>
2025-12-07 13:52:07 +0700

    refactor: three_card_cta component

--- template-parts/components/three_card_cta.php
+++ template-parts/components/three_card_cta.php
@@ -1,16 +1,70 @@
 <?php
 /**
- * Component: Three Card Cta
+ * Component: Three Card CTA
  * Layout: three_card_cta
  */
 
-// $field = get_sub_field('field_name');
+$title = get_sub_field('title');
+
+// Styling
+$section_id    = goodshep_get_section_id();
+$block_classes = goodshep_get_block_classes();
+$bg_style      = goodshep_get_bg_image_style();
+
+// Construct cards array from flat fields
+$cards = [];
+for ( $i = 1; $i <= 3; $i++ ) {
+    $num = str_pad( $i, 2, '0', STR_PAD_LEFT );
+    $cards[] = [
+        'img'   => get_sub_field( "card_{$num}_image_source" ),
+        'title' => get_sub_field( "card_{$num}_title" ),
+        'text'  => get_sub_field( "card_{$num}_text" ),
+    ];
+}
 ?>
 
-<section class="component-three_card_cta py-12">
+<section <?php echo $section_id; ?> class="<?php echo esc_attr( $block_classes ); ?>" style="<?php echo esc_attr( $bg_style ); ?>">
     <div class="container mx-auto px-4">
-        <div class="p-4 border-2 border-dashed border-gray-300 bg-gray-50 rounded-lg text-center">
-            <p class="text-gray-500 font-mono text-sm">Template: template-parts/components/three_card_cta.php</p>
+        
+        <?php if ( $title ) : ?>
+            <h2 class="text-3xl lg:text-4xl font-semibold text-red-600 mb-8"><?php echo esc_html( $title ); ?></h2>
+        <?php endif; ?>
+
+        <div class="grid md:grid-cols-3 gap-8 md:gap-0 md:divide-x divide-gray-200">
+            <?php foreach ( $cards as $card ) : 
+                if ( $card['title'] || $card['text'] ) :
+            ?>
+                <div class="flex flex-col lg:flex-row items-start py-4 md:px-8 first:pl-0 last:pr-0">
+                    
+                    <?php if ( $card['img'] ) : ?>
+                        <div class="flex-none w-14 h-14 mr-4 lg:mr-8 mb-4 lg:mb-0">
+                            <?php 
+                            if ( is_array( $card['img'] ) && isset( $card['img']['ID'] ) ) {
+                                echo wp_get_attachment_image( $card['img']['ID'], 'medium', false, array( 'class' => 'w-full h-full object-contain' ) );
+                            } elseif ( is_numeric( $card['img'] ) ) {
+                                echo wp_get_attachment_image( $card['img'], 'medium', false, array( 'class' => 'w-full h-full object-contain' ) );
+                            } elseif ( is_string( $card['img'] ) ) {
+                                echo '<img src="' . esc_url( $card['img'] ) . '" class="w-full h-full object-contain" alt="">';
+                            }
+                            ?>
+                        </div>
+                    <?php endif; ?>
+
+                    <div class="flex-grow">
+                        <?php if ( $card['title'] ) : ?>
+                            <h3 class="text-2xl font-semibold mb-2"><?php echo esc_html( $card['title'] ); ?></h3>
+                        <?php endif; ?>
+
+                        <?php if ( $card['text'] ) : ?>
+                            <div class="prose max-w-none text-gray-600">
+                                <?php echo wp_kses_post( $card['text'] ); ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+
+                </div>
+            <?php endif; endforeach; ?>
         </div>
+
     </div>
 </section>
