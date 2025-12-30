39d359042e5ce0af37fcf20d36c2d619c61ab541
Slant Dev <build@amazeworx.com>
2025-12-31 04:37:31 +0700

    fix: add sr-only on skip-link

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -7,8 +8,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -16,12 +18,12 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+	<?php wp_body_open(); ?>
 
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'goodshep-theme' ); ?></a>
+	<div id="page" class="site">
+		<a class="skip-link sr-only" href="#primary"><?php esc_html_e('Skip to content', 'goodshep-theme'); ?></a>
 
-	<?php get_template_part( 'template-parts/global/hello-bar' ); ?>
-	<?php get_template_part( 'template-parts/global/site-header' ); ?>
+		<?php get_template_part('template-parts/global/hello-bar'); ?>
+		<?php get_template_part('template-parts/global/site-header'); ?>
 
-    <div id="content" class="site-content">
+		<div id="content" class="site-content">
